5ad3ca2b4496857c6cee21c7d05f7180cb2172d7
Noor Mudassar <noormudassar97@gmail.com>
2023-08-09 16:12:03 -0400

    header showing up on local host

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
-app.use(express.json());
 app.use(cors());
+
+app.use(express.json());
+app.use(express.static("public"));
 
 const {} = require(`./controller`);
 
